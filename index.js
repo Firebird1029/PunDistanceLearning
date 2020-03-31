9f5bdde6ad222a64a1f9bc0df0aef69d9ffde2c7
Jason Tay <jtay20@punahou.edu>
2020-03-31 03:01:23 -1000

    working?

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function extractDataFromTable(nightmare, callback) {
 */
 function getStudentDataViaNightmare (username, password, callback) {
 	console.log(username);
-	let nightmare = new Nightmare({show: false, loadTimeout: 10000});
+	let nightmare = new Nightmare({show: false});
 	nightmare
 		.goto('https://mybackpack.punahou.edu/SeniorApps/facelets/registration/loginCenter.xhtml')
 		.wait('body')
