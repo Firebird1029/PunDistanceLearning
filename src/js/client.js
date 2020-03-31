87d7882483cda906cb9750700241f1ee50ce43ef
Jason Tay <jtay20@punahou.edu>
2020-03-30 19:51:43 -1000

    pdf maker works

--- src/js/client.js
+++ src/js/client.js
@@ -189,41 +189,21 @@ $("#autoSignIn").click(function() {
 
 
 function createPDF() {
-	var pdf = new jsPDF('p', 'pt', 'letter');
-            // source can be HTML-formatted string, or a reference
-            // to an actual DOM element from which the text will be scraped.
-            var source = $('#scheduleTable')[0];
-
-            // we support special element handlers. Register them with jQuery-style 
-            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-            // There is no support for any other type of selectors 
-            // (class, of compound) at this time.
-            var specialElementHandlers = {
-                // element with id of "bypass" - jQuery style selector
-                '#bypassme': function(element, renderer) {
-                    // true = "handled elsewhere, bypass text extraction"
-                    return true
-                }
-            };
-            var margins = {
-                top: 80,
-                bottom: 60,
-                left: 40,
-                width: 522
-            };
-            // all coords and widths are in jsPDF instance's declared units
-            // 'inches' in this case
-            pdf.fromHTML(
-                    source, // HTML string or DOM elem ref.
-                    margins.left, // x coord
-                    margins.top, {// y coord
-                        'width': margins.width, // max width of content on PDF
-                        'elementHandlers': specialElementHandlers
-                    },
-            function(dispose) {
-                // dispose: object with X, Y of the last line add to the PDF 
-                //          this allow the insertion of new lines after html
-                pdf.save('Test.pdf');
-            }
-            , margins);
+	var doc = new jsPDF('p', 'pt');
+	var elem = document.getElementById("scheduleTable");
+	var res = doc.autoTableHtmlToJson(elem);
+	console.log(res.data);
+	doc.autoTable({
+		html: '#scheduleTable',
+		columnStyles: {
+			0: {cellWidth: 50},
+			1: {cellWidth: 75},
+			2: {cellWidth: 75},
+			3: {cellWidth: 75},
+			4: {cellWidth: 75},
+			5: {cellWidth: 75},
+			6: {cellWidth: 75}
+		}
+	});
+	doc.save("table.pdf");
 }
