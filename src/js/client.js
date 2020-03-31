362e032d2899e062c32abc91b0c670d588c80634
Brandon Yee <brandon.yee150@gmail.com>
2020-03-30 15:31:32 -1000

    Delete course button

--- src/js/client.js
+++ src/js/client.js
@@ -12,9 +12,35 @@ $(document).ready(function () {
 	// 	$(".navbar-menu").toggleClass("is-active");
 	// });
 	
-	$("#addCourseBtn").click(() => {
-		$(".makeSchedFormOneCourseGroup:last").clone().insertAfter(".makeSchedFormOneCourseGroup:last");
+	// Start Screen
+	$("#showMakeScheduleScreen").click(() => {
+		$("#makeScheduleScreen").removeClass("is-hidden");
+		$("#addCourseBtn").click();
 	});
+	
+	// Add Course Button
+	$("#addCourseBtn").click(() => {
+		// https://stackoverflow.com/questions/9549643/jquery-clone-not-cloning-event-bindings-even-with-on
+		$(".oneCourseGroupBlank").clone(true, true).removeClass("oneCourseGroupBlank is-hidden").insertAfter(".oneCourseGroup:last");
+	});
+
+	// Delete Course Button
+	// TODO: switch to () => but doesn't work for some reason
+	$(".deleteCourseBtn").click(function () {
+		$(this).closest(".oneCourseGroup").remove();
+	});
+
+	// Submit Make Schedule Form
+	$("#makeScheduleForm").on("submit", () => {
+		// 
+	});
+
+	function compileWebForm () {
+		var courseName = $(".courseName").val(),
+			courseSubject = $(".courseSubjectDropdown").find(":selected").text(),
+			courseStartTime = $(".courseStartTimeDropdown").find(":selected").text(),
+			courseEndTime = $(".courseEndTimeDropdown").find(":selected").text();
+	}
 });
 
 var socket = io.connect();
