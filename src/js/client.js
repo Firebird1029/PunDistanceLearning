90ae0264b653a9ced536e9eaedd0fcd7796b5386
Brandon Yee <brandon.yee150@gmail.com>
2020-03-30 14:48:46 -1000

    Basic Form Layout

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,19 @@
 "use strict"; /* eslint-env browser */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
+const debug = true;
 
 // Navbar Burger
 $(document).ready(function () {
-	$(".navbar-burger").click(function () {
-		$(".navbar-burger").toggleClass("is-active");
-		$(".navbar-menu").toggleClass("is-active");
+	if (debug) {
+		$("#makeScheduleScreen").removeClass("is-hidden");
+	}
+
+	// $(".navbar-burger").click(function () {
+	// 	$(".navbar-burger").toggleClass("is-active");
+	// 	$(".navbar-menu").toggleClass("is-active");
+	// });
+	
+	$("#addCourseBtn").click(() => {
+		$(".makeSchedFormOneCourseGroup:last").clone().insertAfter(".makeSchedFormOneCourseGroup:last");
 	});
 });
 
